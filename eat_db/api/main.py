02f38a7f0a6ae2fb944c73e8795d1af15c629739
Gabriel Gore <gabriel59kg@gmail.com>
2020-01-26 17:42:15 -0500

    refactor, add logger, pydantic model

--- eat_db/api/main.py
+++ eat_db/api/main.py
@@ -4,31 +4,65 @@ eat_db.api.app.py
 fastapi app module.
 """
 import enum
+import logging
 
 import fastapi
+import pydantic
+
+LOGGER = logging.getLogger("api")
+LOGGER.setLevel(logging.DEBUG)
+
+DEMO_NOSQL_DB = {
+    "fridge": [
+        "spam",
+        "bacon",
+        "eggs",
+        "steak",
+        "chinese",
+        "pizza",
+        "mexican",
+        "cheese",
+        "hotdogs",
+        "hamburgers",
+        "yogurt",
+    ],
+    "freezer": ["steak", "ice-cream", "peas", "pizza", "fries"],
+}
+
+
+def get_db(**kwargs):
+    LOGGER.debug(f"get_db() kwargs:{kwargs}")
+    return DEMO_NOSQL_DB
+
+
+class BinTypes(str, enum.Enum):
+    fridge = "fridge"
+    freezer = "freezer"
+    pantry = "pantry"
+    extra_freezer = "extra-freezer"
+
+
+class Bin(pydantic.BaseModel):
+    name: str
+    bin_type: BinTypes
+    description: str = None
+
+    @pydantic.validator("name")
+    def unique_name(cls, v):
+        if v in get_db():
+            raise ValueError(f"name must be unique, {v} already exists.")
+
 
 app = fastapi.FastAPI()
 
 
-class Bins(str, enum.Enum):
-    fridge = "fridge"
-    freezer = "freezer"
-    extra_freezer = "extra-freezer"
-
-
-db = {
-    Bins.fridge: ["spam", "bacon", "eggs", "steak"],
-    Bins.freezer: ["steak", "ice-cream", "peas"],
-}
-
-
 @app.get("/")
 async def root():
-    return {"message": {k: len(v) for (k, v) in db.items()}}
+    return {"message": {k: len(v) for (k, v) in get_db().items()}}
 
 
 @app.get("/bin/{name}")
-async def check_bin(name: Bins = Bins.fridge, skip: int = 0, limit: int = 10):
+async def check_bin(name: BinTypes = BinTypes.fridge, skip: int = 0, limit: int = 10):
     """Check bins for contents."""
-    bin_contents = db.get(name, [])[skip:limit]
+    bin_contents = get_db().get(name, [])[skip:limit]
     return bin_contents
