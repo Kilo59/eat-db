0549812a9cbeba58e4ead08611a45c2b8c6a2de4
Gabriel Gore <gabriel59kg@gmail.com>
2020-01-26 13:50:21 -0500

    Bins model as path parameters

--- eat_db/api/main.py
+++ eat_db/api/main.py
@@ -3,11 +3,30 @@ eat_db.api.app.py
 ~~~~~~~~~~~~~~~~~
 fastapi app module.
 """
+import enum
+
 import fastapi
 
 app = fastapi.FastAPI()
 
 
+class Bins(str, enum.Enum):
+    fridge = "fridge"
+    freezer = "freezer"
+    extra_freezer = "extra-freezer"
+
+
+db = {
+    Bins.fridge: ["spam", "bacon", "eggs", "steak"],
+    Bins.freezer: ["steak", "ice-cream", "peas"],
+}
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": {k: len(v) for (k, v) in db.items()}}
+
+
+@app.get("/bin/{name}")
+async def check_bin(name: Bins):
+    return db.get(name, [])
